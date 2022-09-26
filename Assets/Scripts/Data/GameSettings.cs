b924ad5954232fc0069db408876b68647c6c26c3
Lekret <lenicraft@mail.ru>
2022-09-26 17:22:20 +0300

    Basic logic

---
+++ Assets/Scripts/Data/GameSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(menuName = "StaticData/GameSettings", fileName = "GameSettings")]
+    public class GameSettings : ScriptableObject
+    {
+        [SerializeField] private int _mapWidth;
+        [SerializeField] private int _mapHeight;
+        [SerializeField] private int _mirageCount;
+        [SerializeField] private int _heroSwitchCount;
+
+        public int MapWidth => _mapWidth;
+        public int MapHeight => _mapHeight;
+        public int MirageCount => _mirageCount;
+        public int HeroSwitchCount => _heroSwitchCount;
+    }
+}
---
+++ Assets/Scripts/Data/HeroData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+[Serializable]
+public class HeroData
+{
+    public int Initiative;
+    public int ActionDiameter;
+    public int Laziness;
+    public int Search;
+    public int Speed;
+    public int Collection;
+    public int Damage;
+    public int DamagePerTurn;
+    public int Shield;
+    public int Health;
+    public int Ptsd;
+}
