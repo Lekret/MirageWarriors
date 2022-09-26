a11a7946ce81bab2442d1e9be82094701dfb1d6f
Lekret <lenicraft@mail.ru>
2022-09-26 18:15:27 +0300

    Hero init

--- Assets/Scripts/Hero/Hero.cs
+++ Assets/Scripts/Hero/Hero.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using Data;
+using UnityEngine;
 
 namespace Hero
 {
     public class Hero : MonoBehaviour
     {
-    
+        [SerializeField] private CircleCollider2D _actionArea;
+
+        public void Init(HeroData data)
+        {
+            _actionArea.radius = data.ActionDiameter / 2f;
+        }
     }
 }
