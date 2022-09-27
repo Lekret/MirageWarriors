cfc20455eb01093da62fcd9975e6595fa7103ed5
Lekret <lenicraft@mail.ru>
2022-09-27 08:07:38 +0300

    HeroData default values

--- Assets/Scripts/StaticData/HeroData.cs
+++ Assets/Scripts/StaticData/HeroData.cs
@@ -7,15 +7,15 @@ namespace StaticData
     [Serializable]
     public class HeroData
     {
-        public int Initiative;
-        public int ActionDiameter;
-        public int Enthusiasm;
-        public int Search;
-        public int Speed;
-        public int Collection;
-        public int Damage;
-        public int Shield;
-        public int Health;
-        public int Cooldown;
+        public int Initiative = 10;
+        public int ActionDiameter = 100;
+        public int Enthusiasm = 7800;
+        public int Search = 500;
+        public int Speed = 100;
+        public int Collection = 20;
+        public int Damage = 100;
+        public int Shield = 500;
+        public int Health = 500;
+        public int Cooldown = 4;
     }
 }
