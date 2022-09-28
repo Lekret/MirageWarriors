2fc9635ac86a7986bb4b191a369a5400dfee2bc3
Lekret <lenicraft@mail.ru>
2022-09-28 15:55:43 +0300

    MirageService

---
+++ Assets/Scripts/Services/MirageService/IMirageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Services.MirageService
+{
+    public interface IMirageService
+    {
+        event Action<int> PlayerMirageChanged;
+        event Action<int> EnemyMirageChanged;
+        event Action PlayerWon;
+        event Action EnemyWon;
+        void AddToPlayer(int mirage);
+        void AddToEnemy(int mirage);
+    }
+}
---
+++ Assets/Scripts/Services/MirageService/MirageService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using StaticData;
+
+namespace Services.MirageService
+{
+    public class MirageService : IMirageService
+    {
+        private readonly GameSettings _gameSettings;
+
+        public MirageService(GameSettings gameSettings)
+        {
+            _gameSettings = gameSettings;
+        }
+
+        public int PlayerMirage { get; private set; }
+        public int EnemyMirage { get; private set; }
+        public event Action<int> PlayerMirageChanged;
+        public event Action<int> EnemyMirageChanged;
+        public event Action EnemyWon;
+        public event Action PlayerWon;
+
+        public void AddToPlayer(int mirage)
+        {
+            PlayerMirage += mirage;
+            PlayerMirageChanged?.Invoke(PlayerMirage);
+            if (IsMirageAboveHalf(mirage))
+                PlayerWon?.Invoke();
+        }
+
+        public void AddToEnemy(int mirage)
+        {
+            EnemyMirage += mirage;
+            EnemyMirageChanged?.Invoke(EnemyMirage);
+            if (IsMirageAboveHalf(mirage))
+                EnemyWon?.Invoke();
+        }
+
+        private bool IsMirageAboveHalf(int mirage)
+        {
+            return mirage > _gameSettings.MirageCount / 2f;
+        }
+    }
+}
