470f29cd7b34a69bb64b53a03bfd4d68376e6421
Lekret <lenicraft@mail.ru>
2022-09-28 10:48:27 +0300

    Action area count

--- Assets/Scripts/Heroes/ActionArea.cs
+++ Assets/Scripts/Heroes/ActionArea.cs
@@ -6,6 +6,8 @@ namespace Heroes
     public class ActionArea : MonoBehaviour
     {
         [SerializeField] private CircleCollider2D _collider;
+        [SerializeField] private SpriteRenderer _areaRenderer;
+        [SerializeField] private float _spriteSizeMultiplier = 2.3f;
         
         private readonly HashSet<Hero> _nearestHeroes = new HashSet<Hero>();
         public IEnumerable<Hero> NearestHeroes => _nearestHeroes;
@@ -13,6 +15,8 @@ namespace Heroes
         public void SetDiameter(int diameter)
         {
             _collider.radius = diameter / 2f;
+            var side = _collider.radius * _spriteSizeMultiplier;
+            _areaRenderer.size = new Vector2(side, side);
         }
         
         private void OnTriggerEnter2D(Collider2D other)
